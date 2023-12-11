439acc3aa104c43313745f17a37e85c7f9be9291
Akhil Iyengar <akhiliyengar2004@gmail.com>
2023-12-11 01:30:09 -0500

    Update Alias.jsx
    ssss

--- src/componets/Alias.jsx
+++ src/componets/Alias.jsx
@@ -11,14 +11,15 @@ function Alias() {
           <h1>Conduct user research in<br></br> seconds.</h1>
           <p> Simulate user interviews with AI personas trained to think, speak,<br></br> and act just like your customers. </p>
           <div className="mbutton mt-5">
-          <a href="https://calendly.com/aliasintroakhil/30min" target="_blank">
-              <button>Schedule a demo today</button>
-          </a>
+              <a href="https://calendly.com/aliasintroakhil/30min" target="_blank">
+                <button>Schedule a demo today</button>
+              </a>
+              &nbsp; &nbsp; 
+              <a href="https://airtable.com/appT0poZ0DgKnUz4A/pagIVvmvDM2KN6Xb8/form" target="_blank">
+                <button>Get Started &nbsp; <i className="fa-solid fa-chevron-right"></i></button>
+              </a>
+            </div>
           </div>
-          </div>
-          <a href="https://airtable.com/appT0poZ0DgKnUz4A/pagIVvmvDM2KN6Xb8/form" target="_blank">
-              <button>Get Started &nbsp; <i class="fa-solid fa-chevron-right"></i></button>
-          </a>
           </div>
         </section>
     </div>
