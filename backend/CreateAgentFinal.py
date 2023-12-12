8f97e2d79a720a107f748f9db557591aceb6e4b8
Akhil Iyengar <akhiliyengar2004@gmail.com>
2023-12-11 20:40:22 -0500

    Update CreateAgentFinal.py
    sss

--- backend/CreateAgentFinal.py
+++ backend/CreateAgentFinal.py
@@ -1,10 +1,11 @@
-from backend import peopledatalabsretrieval
+import peopledatalabsretrieval
 from langchain_experimental import generative_agents
-from backend.twitter import user_lookup
-from backend.twitter import user_tweets
-from backend.reddit import redditapi
-from backend.reddit import redditusers
-# import promptLLMmemories
+from twitter import user_lookup
+from twitter import user_tweets
+from reddit import redditapi
+from reddit import redditusers
+from reddit import additionalredditmemories
+import promptLLMmemories
 from langchain_experimental import generative_agents
 from datetime import datetime, timedelta
 from typing import List
@@ -20,12 +21,13 @@ from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
 import json
 import re
-from backend import google_search_results
+import google_search_results
 import random
-# from backend.retrieve_agent import push_agent_info
-# from backend.load_agent_database import LoadAgent
+from retrieve_agent import push_agent_info
+from load_agent_database import LoadAgent
 import datetime
-
+import retrieve_agent
+from langchain.llms import OpenAI
 
 def create_new_memory_retriever():
     """Create a new vector store retriever unique to the agent."""
@@ -96,6 +98,8 @@ def create_and_store_agent(description,age,job1):
     llm=LLM,
     memory=agent_memory,
 )
+  tweets=[]
+  likedtweets=[]
   if(persondict['twitter']!='false'):
    try:
       twitter_username=persondict["twitter"]
@@ -110,54 +114,99 @@ def create_and_store_agent(description,age,job1):
   else: 
      tweets=[]
      likedtweets=[]
-  subredditslist= redditusers.get_commmon_subreddit(interestslist)
-  user= redditusers.get_users(interestslist,subredditslist)
-  comments= user[1]
-  username=user[0]
-  totallist= redditusers.find_most_relevant_submissions(interestslist,comments,username)
-  for val in tweets:
-     totallist.append(val)
-  for val1 in likedtweets:
-     totallist.append(val1)
+
+  (user,coms,sums)= additionalredditmemories.determine_most_similair_redditor(interestslist,additionalredditmemories.get_common_subreddit_interestlist(interestslist))
+  (foms,fubs)=additionalredditmemories.gpt_redditor(user,coms,sums)
+  # subredditslist= redditusers.get_commmon_subreddit(interestslist)
+  # user= redditusers.get_users(interestslist,subredditslist)
+  # comments= user[1]
+  # username=user[0]
+  # totallist= redditusers.find_most_relevant_submissions(interestslist,comments,username)
+  totallist=[]
+  # for val in tweets:
+  #    totallist.append(val)
+  # for val1 in likedtweets:
+  #    totallist.append(val1)
+  for com in foms: 
+     totallist.append(com)
+  for sub in fubs: 
+     totallist.append(sub)
   for memory in totallist:
     print("looped memory"+memory)
     agent.memory.add_socialmedia_memory(memory)
 
-  productstring= agent.memory.search_prodct_questions(product,agent.status)
-  interestslist=agent.interests.split(';')
   
+  
+  # productstring= agent.memory.search_prodct_questions(product,agent.status)
+  # interestslist=agent.interests.split(';')
+  # actual_product_list= productstring[:3]
+  totalquerylist=[]
+  for interest in interestslist: 
+     links=agent.search_prodct_questions(interest,"test")
+     totalquerylist.append(links)
 
-
-
-  actual_product_list= productstring[:3]
-  print(actual_product_list)
+  totalquerylist=[item for sublist in totalquerylist for item in sublist]
   big_url_list=[]
   count=0
-  for query in actual_product_list: 
+  for query in totalquerylist: 
     if(count>2): 
         break 
-    print("Finding queries")
+    # print("Finding queries")
     big_url_list.append(google_search_results.api_results(query))
 
   
   big_url_list = [item for sublist in big_url_list for item in sublist]
   print("overall big url list"+ str(big_url_list))
-  random.shuffle(big_url_list)
-  big_url_list=big_url_list[:10]
+  # random.shuffle(big_url_list)
+  # big_url_list=big_url_list[:10]
+
   text_url_list=google_search_results.urls_to_summarizedtext(big_url_list)
-  print("finished summarizing")
-  print("Overall total url list"+ str(big_url_list))
-  if(len(text_url_list)>10): 
-      random.shuffle(text_url_list)
-      text_url_list= text_url_list[:10]
-  for text in text_url_list: 
-    print("looped products")
-    agent.memory.add_product_memory(text)
-    print("")
+  results=[item for sublist in text_url_list for item in sublist]
+  reduced_list = [results[i] +" New Article"+ results[i+1]+results[i+2]+results[i+4]+results[i+5] for i in range(0, len(results)-5, 5)]
+  totlist=agent.analysis_of_product(reduced_list)
+  for mem in totlist: 
+    print(mem)
+    if(isinstance(mem,str)):
+      agent.memory.add_memory(mem)
+    else:
+       print("not mem")
 
   print("memoryinginginginign")
-  agent.product_to_memory(product)
   return (agent,gender,job1)
+
+
+
+
+
+
+
+
+
+
+# (agent,gender,job1)=create_and_store_agent("Someone who is very interested in technology and loves to watch movies and tv shows",25,"developer")
+# print(agent.generate_question_response("What do you like doing"))
+# print(agent.generate_question_response("what are the most important thigns in your life"))
+# agent_memory= agent.memory.memory_retriever.dict()
+# agent_soc_memory=agent.memory.social_media_memory.dict()
+# del agent_memory['vectorstore']
+# del agent_soc_memory['vectorstore']
+# retrieve_agent.push_agent_info(agent.name,agent.age,agent.status,json.dumps(str(agent_memory)),json.dumps({}),'rbpeddu@gmail.com',json.dumps(str(agent_soc_memory)),agent.education_and_work,json.dumps(agent.memory.personalitylist),agent.interests,gender,"senior developer","Technology enthusiast who is interested in natrue")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # print(create_and_store_agent("Wants to buy a new computer",34,"developer"))
