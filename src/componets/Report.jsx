53ea57129a758e697102e0cbb2d3c0a607d29e78
Akhil Iyengar <akhiliyengar2004@gmail.com>
2024-01-09 19:23:20 -0800

    e

--- src/componets/Report.jsx
+++ src/componets/Report.jsx
@@ -169,7 +169,7 @@ return (
                         {dictionaryLength > 0 ? (
                             <div style={{ display: 'flex', justifyContent: 'center' }}>
                                 {Array.from(JSON.parse(reportFinal["Scores"])).map((score, index) => (
-                                    <div key={index} style={{ marginRight: index === 1 ? '120px' : '100px', textAlign: 'center' }}>
+                                    <div key={index} style={{ marginRight: index === 1 ? '120px' : '150px', textAlign: 'center' }}>
                                         <ProgressCircle progress={parseFloat(score).toFixed(2)} />
                                         <p style={{ marginTop: '5px' }}>
                                             {parseFloat(score).toFixed(2)} - {getMetricName(index)}
